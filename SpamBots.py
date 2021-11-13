ed5ed36f030b5c38d057083f7f14edf7dfcee44e
MAHABALI <85936031+mahabalix@users.noreply.github.com>
2021-11-13 22:08:22 +0530

    Update SpamBots.py

--- SpamBots.py
+++ SpamBots.py
@@ -724,7 +724,7 @@ async def _(e):
 async def ping(e):
     if e.sender_id in SMEX_USERS:
         start = datetime.now()
-        text = "READY BOSS"
+        text = "LUND"
         event = await e.reply(text, parse_mode=None, link_preview=None)
         end = datetime.now()
         ms = (end - start).microseconds / 1000
